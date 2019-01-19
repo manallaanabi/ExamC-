6e09cfc2d7af3b003b6f7670ab6eea713e433797
manallaanabi <46848871+manallaanabi@users.noreply.github.com>
2019-01-20 00:17:08 +0100

    Add files via upload

---
+++ YTM.h
@@ -0,0 +1,22 @@
+//
+// Created by Manal on 19/01/2019.
+//
+
+#ifndef UNTITLED3_YTM_H
+#define UNTITLED3_YTM_H
+
+#endif //UNTITLED3_YTM_H
+void YTM(){
+int r,a,b,i;
+
+    a=1;
+    b=2;
+    for (i=0; i<=10; i=i+1) {
+        r=a;
+        r=b;
+        r = b - (a - (b - a)) / (b - a);
+
+    }
+    cout << "rate: "  <<  r << "\n";
+}
+

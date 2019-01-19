569f44d57875e92b5401794ab0e2b215f59e4061
manallaanabi <46848871+manallaanabi@users.noreply.github.com>
2019-01-19 23:39:59 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include "CalculOfPmt.h"
+#include "calculOfPV.h"
+#include "calculOfn.h"
+#include "CalculOfr.h"
+using namespace std ;
+
+int main() {
+   // CalculPmt();
+    //CalculPV();
+    //Calcul();
+    Cal();
+}
+
+
+
